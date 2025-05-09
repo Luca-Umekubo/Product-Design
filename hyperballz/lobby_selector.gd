a0769a162ccdb516450ce0a2c8e4147613c6204d
Tyler <tanderson1@hwemail.com>
2025-05-08 22:49:31 -0700

    WORKING CROSS-COMPUTER/Minor Updates
    Most importantly, cross-computer gameplay is confirmed working on a home, local network. Also some minor updates & removing a line of debug logging.

--- hyperballz/lobby_selector.gd
+++ hyperballz/lobby_selector.gd
@@ -3,6 +3,5 @@ extends Control
 func _on_join_button_pressed():
 	var ip = $VBoxContainer/IPLineEdit.text
 	var port = int($VBoxContainer/PortLineEdit.text)
-	print("join")
 	Network.join_server(ip, port)
 	get_tree().change_scene_to_file("res://Game.tscn")
