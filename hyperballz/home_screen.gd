e7af27288dd5f9573a158c0a468836e357caaa1f
Tyler <tanderson1@hwemail.com>
2025-05-09 08:16:39 -0700

    Updated Hoting port to 4242

--- hyperballz/home_screen.gd
+++ hyperballz/home_screen.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_host_pressed():
-	Network.start_server(1234, 10)  # Port 1234, max 10 clients
+	Network.start_server(4242, 10)  # Port 4242, max 10 clients
 	get_tree().change_scene_to_file("res://Game.tscn")
 
 func _on_join_pressed():
