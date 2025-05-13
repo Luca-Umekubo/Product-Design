47bc54e9813f0e5937979c2eeeafc53459872af5
Brody <beddy1@hwemail.com>
2025-05-13 09:07:19 -0700

    fixed endscreen functionality

--- hyperballz/EndScreen.gd
+++ hyperballz/EndScreen.gd
@@ -3,8 +3,9 @@ extends Control
 signal leave_pressed
 signal play_again_pressed
 
-func _on_Leave_Game_pressed():
-	emit_signal("leave_pressed")
+func _on_leave_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://HomeScreen.tscn")
 
-func _on_Play_Again_pressed():
-	emit_signal("play_again_pressed")
+
+func _on_play_again_pressed() -> void:
+	get_tree().change_scene_to_file("res://LobbySelector.tscn")
